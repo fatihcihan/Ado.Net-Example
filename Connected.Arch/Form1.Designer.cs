8bc10824da36f02e77b74d75e63af8621c917e0d
fatihcihan666 <fatihcihan666@gmail.com>
2022-11-19 00:48:47 +0300

    Add project files.

---
+++ Connected.Arch/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Connected.Arch
+{
+    partial class ProductForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listProcudts = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listCategories = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listProcudts
+            // 
+            this.listProcudts.FormattingEnabled = true;
+            this.listProcudts.ItemHeight = 16;
+            this.listProcudts.Location = new System.Drawing.Point(12, 82);
+            this.listProcudts.Name = "listProcudts";
+            this.listProcudts.Size = new System.Drawing.Size(475, 324);
+            this.listProcudts.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(142, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Products";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(789, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Categories";
+            // 
+            // listCategories
+            // 
+            this.listCategories.FormattingEnabled = true;
+            this.listCategories.ItemHeight = 16;
+            this.listCategories.Location = new System.Drawing.Point(608, 82);
+            this.listCategories.Name = "listCategories";
+            this.listCategories.Size = new System.Drawing.Size(475, 324);
+            this.listCategories.TabIndex = 3;
+            // 
+            // ProductForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1311, 557);
+            this.Controls.Add(this.listCategories);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listProcudts);
+            this.Name = "ProductForm";
+            this.Text = "ProductForm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listProcudts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listCategories;
+    }
+}
+
---
+++ Connected.Arch/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connected.Arch
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetProducts();
+            GetCategories();
+        }
+
+
+        public void GetProducts()
+        {
+
+            // Sql Auth.
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = "Server=localhost;Database=Northwind;UID=sa;Password=1;";
+
+            // Windows Auth.
+            //sqlConnection.ConnectionString = "Server=localhost;Database=Northwind;Integrated_Security=true;";
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandText = "Select * from Products";
+            sqlCommand.Connection = sqlConnection;
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                var productName = sqlDataReader["ProductName"];
+                var unitPrice = sqlDataReader["UnitPrice"];
+                var quantityPerUnit = sqlDataReader["QuantityPerUnit"];
+
+                listProcudts.Items.Add(string.Format("{0}-{1}-{2}", productName, unitPrice, quantityPerUnit));
+            }
+            sqlConnection.Close();
+        }
+
+        public void GetCategories()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=localhost;Database=Northwind;UID=sa;Password=1;");
+
+            SqlCommand sqlCommand = new SqlCommand("Select * from Categories", sqlConnection);
+            sqlConnection.Open();
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+            while (sqlDataReader.Read())
+            {
+                var categoryName = sqlDataReader["CategoryName"];
+                var description = sqlDataReader["Description"];
+
+                listCategories.Items.Add(string.Format("{0}-{1}", categoryName, description));
+            }
+            sqlConnection.Close();
+        }
+    }
+}
